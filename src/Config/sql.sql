8a067be134ef2d978020985f148c6a1e0c7041ac
giovani.cassiano <giovani.cassiano@riosoft.com.br>
2019-02-15 09:24:02 -0200

    [upt] melhorias na estrtura de pastas e arquivos

---
+++ src/Config/sql.sql
@@ -0,0 +1,16 @@
+create database CRUD_Simple;
+use CRUD_Simple;
+
+create table Usuario
+(
+	id 		int			 not null primary key auto_increment,
+	nome 	varchar(50) not null,
+	email varchar(50) not null unique,
+	senha varchar(10) not null
+);
+
+create table Produtos
+(
+	id        int         not null primary key auto_increment,
+	descricao varchar(100) not null
+);
---
+++ src/Database/Connection.class.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Class Connection
+ */
+class Connection
+{
+    private static $instance = null;
+    const OPTIONS = [
+      \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
+      \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+    ];
+
+    /**
+     * Connection constructor.
+     * @throws Exception
+     */
+    public static function connect()
+    {
+        try {
+            if(!self::$instance) {
+                $result = self::getAccess();
+                self::$instance = new \PDO(
+                    'mysql:dbname=' . $result['DATABASE'] . ';host=' . $result['HOST'],
+                    $result['USER'],
+                    $result['PASSWORD'],
+                    self::OPTIONS
+                );
+                return self::$instance;
+            }
+            return self::$instance;
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage(), 500);
+        }
+    }
+
+    /**
+     * @return array|bool
+     */
+    private static function getAccess()
+    {
+        $result = parse_ini_file(__DIR__ . '/../Config/Database.ini');
+        return $result;
+    }
+
+    /**
+     * Fechar connexão
+     */
+    public static function closeConnection()
+    {
+        self::$instance = null;
+    }
+}
